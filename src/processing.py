10d19831efcd290f7d5601548b9946fe473659a5
ksenichvlasova <vlasovaxenia2015@gmail.com>
2025-11-27 14:38:22 +0300

    add function

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,11 @@
-def filter_by_state(transactions, state='EXECUTED'):
-def sort_by_date(transactions, reverse=False):
+def filter_by_state(transactions: list, state: str = 'EXECUTED') -> list:
+    new_transactions = []
+    for transaction in transactions:
+        if transaction.get("state") == state:
+            new_transactions.append(transaction)
+    return new_transactions
+
+
+def sort_by_date(transactions: list, reverse: bool = False) -> list:
+    new_transactions = sorted(transactions, key=lambda x: x.get("date"), reverse=reverse)
+    return new_transactions
