b9f09dcb67dcdd50b2bea108c78f18b8c79acff3
ksenichvlasova <vlasovaxenia2015@gmail.com>
2025-12-02 15:04:09 +0300

    fix

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(transactions: list, state: str = 'EXECUTED') -> list:
+    """Функция для фильтрации списка транзакций по статусу"""
     new_transactions = []
     for transaction in transactions:
         if transaction.get("state") == state:
@@ -7,5 +8,6 @@ def filter_by_state(transactions: list, state: str = 'EXECUTED') -> list:
 
 
 def sort_by_date(transactions: list, reverse: bool = False) -> list:
+    """Функция для сортировки списка транзакций по дате"""
     new_transactions = sorted(transactions, key=lambda x: x.get("date"), reverse=reverse)
     return new_transactions
