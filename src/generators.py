acacb42bf177c2231fe719ae3ae50bce28d005a1
ksenichvlasova <vlasovaxenia2015@gmail.com>
2025-12-19 12:53:17 +0300

    fix

--- src/generators.py
+++ src/generators.py
@@ -16,3 +16,26 @@ def filter_by_currency(
     for tx in transactions:
         if tx.get("currency") == currency:
             yield tx
+
+
+def card_number_generator(
+        start: int,
+        end: int
+) -> Iterator[str]:
+
+    """
+    Генератор номеров банковских карт от start до end в формате XXXX XXXX XXXX XXXX.
+    """
+    for num in range(start, end + 1):
+        # Преобразуем номер карты в строку из 16 цифр
+        card_number = f"{num:016d}"
+        # Разбиваем на блоки по 4 цифры для правильного отображения
+        new_card_number = " ".join([card_number[i:i + 4] for i in range(0, 16, 4)])
+        yield new_card_number
+
+
+def transaction_descriptions(
+        transactions: Iterable[Dict[str, Any]],
+) -> Iterator[Dict[str, Any]]:
+    for tx in transactions:
+        yield tx.get('description')
